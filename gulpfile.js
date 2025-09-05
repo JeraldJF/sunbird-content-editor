b292eefa33e0c3b5a356ed87b62e8fd5e8835cfe
JeraldJF <jeraldj@sanketika.in>
2025-09-05 12:53:16 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -355,6 +355,7 @@ gulp.task('zipDev', ['minifyDev', 'injectDev'], function () {
 gulp.task('buildDev', ['minifyDev', 'injectDev', 'zipDev', "cachebust"]);
 
 var corePlugins = [
+    "org.ekstep.colorpicker-1.0",
     "org.ekstep.config-1.0",
     "org.ekstep.readalongbrowser-1.0",
     "org.ekstep.assetbrowser-1.4"
