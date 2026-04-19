bb01f9ca87a96b9c904e373360b561b94774a648
JeraldJF <jeraldj@sanketika.in>
2026-04-19 17:55:46 +0530

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -330,11 +330,11 @@ module.exports = (env, argv) => {
                     },
                     {
                         from: './app/bower_components/jquery/dist/jquery.min.js',
-                        to: 'scripts/jquery.min.js'
+                        to: 'jquery.min.js'
                     },
                     {
                         from: './app/bower_components/semantic/dist/semantic.min.js',
-                        to: 'scripts/semantic.min.js'
+                        to: 'semantic.min.js'
                     }
                 ]
             }),
